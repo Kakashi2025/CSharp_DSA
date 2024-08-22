3905b3683d6e21edd1edad0cab049808b7297b32
Kakashi2025 <147341160+Kakashi2025@users.noreply.github.com>
2024-08-22 22:24:50 +0530

    Create Pattern1.cs

---
+++ CSharp_DSA/Pattern1.cs
@@ -0,0 +1,17 @@
+class Solution
+    {
+        //Complete this function
+        //Function to print square of the given number.
+        public void printSquare(int n)
+        {
+            for(int i=0; i<n; i++){
+                
+                for(int j=0; j< n-1; j++){
+                    Console.Write("* ");
+                    
+                }
+                Console.WriteLine("*");
+            }
+            //Your code here
+        }
+    }
