853fab1962813bddca186ef8e9332c7124b73f95
Kakashi2025 <147341160+Kakashi2025@users.noreply.github.com>
2024-08-22 23:29:58 +0530

    Create pattern5.cs

---
+++ CSharp_DSA/pattern5.cs
@@ -0,0 +1,19 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int n = 5;
+          for(int i=0; i<n; i++){
+            
+            for(int j = i+1; j<n;j++){
+                Console.Write("* ") ;
+            }
+            Console.WriteLine("*");
+        }
+        }
+    }
