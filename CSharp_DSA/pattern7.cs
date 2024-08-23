78c4513194707e89be929c193cfe8290dab3e1da
Kakashi2025 <147341160+Kakashi2025@users.noreply.github.com>
2024-08-23 22:44:48 +0530

    Create pattern7.cs

---
+++ CSharp_DSA/pattern7.cs
@@ -0,0 +1,29 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int n =6;
+        for(int i=0; i<n; i++){
+            for(int j =0; j < n-i-1; j++){
+                Console.Write(" ");
+            }
+            for(int j=0; j< 2 * i + 1; j++){
+                Console.Write("*");
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
+
+// Output for n =6
+//      * 
+//     *** 
+//    ***** 
+//   ******* 
+//  ********* 
+// *********** 
